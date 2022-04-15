06a7967cdc47b9f9602834ad11f1bbd8603b573f
Jerry Au <hoikanau123@gmail.com>
2022-04-15 12:34:27 -0700

    12.34 4.15

--- app/routes/default.py
+++ app/routes/default.py
@@ -26,9 +26,15 @@ def solutions():
 def humanInter():
     return render_template('humanInter.html')
 
-@app.route('/choice')
-def choice():
-    return render_template('CYOA/choice.html')
+@app.route('/choice1')
+def choice1():
+    return render_template('CYOA/choice1.html')
+@app.route('/choice2')
+def choice2():
+    return render_template('CYOA/choice2.html')
+@app.route('/choice3')
+def choice3():
+    return render_template('CYOA/choice3.html')
 
 @app.route('/blank')
 def blank():
