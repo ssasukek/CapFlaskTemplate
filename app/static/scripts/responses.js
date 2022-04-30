2640b29f9767c6d3bb79a6f18c11e1e6f706f36c
AndyWKLiu <liuandy606@gmail.com>
2022-04-30 16:26:45 -0700

    "options" to bot

--- app/static/scripts/responses.js
+++ app/static/scripts/responses.js
@@ -1,8 +1,11 @@
 function getBotResponse(input){
-    if (input == "hello" || "hi" || "hey" || "sup" || "greetings"){
+    if (input == "hello" || input == "hi" || input == "hey" || input == "sup" || input == "greetings"){
         return "hello";
     }
+    if(input == "options"){
+        return "here's links to our climate page : https://127.0.0.1:5000/climatechange . A link to our about-us page : https://127.0.0.1:5000/aboutus . ";
+    }
     else{
-        return "I don't undestand what you're saying :("
+        return "I don't undestand what you're saying :("; 
     }
 }
